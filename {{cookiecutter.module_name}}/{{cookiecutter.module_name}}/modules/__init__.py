fbc37337d185cdcfc01f987478b4424b91cd2a7d
Brandon Braner <brandon.braner@gmail.com>
2023-08-26 19:43:48 -0500

    added check for file for auto import before trying to import it

--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/modules/__init__.py
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/modules/__init__.py
@@ -8,14 +8,21 @@ api_router = APIRouter()
 
 def auto_mount_routes():
     current_folder = f"{os.getcwd()}/{{cookiecutter.module_name}}/modules"
-    subfolders = [f for f in os.listdir(current_folder) if (os.path.isdir(os.path.join(current_folder, f)) and not f.startswith("__"))]
+    subfolders = [
+        f
+        for f in os.listdir(current_folder)
+        if (os.path.isdir(os.path.join(current_folder, f)) and not f.startswith("__"))
+    ]
 
     for folder in subfolders:
         routes = ["api", "web"]
         name = f"{{cookiecutter.module_name}}.modules.{folder}"
 
         for route in routes:
-            router = importlib.import_module(f"{name}.{route}_routes")
+            path = f"{name}.{route}_routes"
+            if not os.path.exists(f"{path}.py"):
+                continue
+            router = importlib.import_module(path)
             if route == "api":
                 api_router.include_router(router.router)
             elif route == "web":
